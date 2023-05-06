7eea01748ee4ca2bd0006d9328493d5675cb861a
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-05-06 23:29:40 +0530

    fixed

--- src/Components/Feeds.js
+++ src/Components/Feeds.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Box,Typography,Stack } from '@mui/material'
+import Sidebar from './Sidebar'
 const Feeds = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
     <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+      <Sidebar />
       
       <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
         Copyright © 2022 JSM Media
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,6 +2,8 @@ import { Stack } from '@mui/material'
 import React from 'react'
 import { categories } from '../Utils/Constants'
 
+const selected = 'New'
+
 const Sidebar = () => {
   return (
     <Stack
@@ -13,11 +15,11 @@ const Sidebar = () => {
     }}
   >
     {categories.map((category) => (
-      <button>
-        <span>
+      <button className="category-btn" style={{background:category.name === selected && 'red',color: 'white'}} key={category.name}>
+        <span style={{marginRight: 15}}>
           {category.icon}
         </span>
-        <span>
+        <span style={{opacity: category.name === selected ? '1' : '0.9'}}>
           {category.name}
         </span>
       </button>
