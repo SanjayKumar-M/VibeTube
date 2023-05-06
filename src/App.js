29597ee0b315e0da63fae165a16a1c7579047c94
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-05-06 20:23:44 +0530

    Feeds

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import './App.css';
 import { Box } from '@mui/material';
-
+import Navbar from './Components/Navbar';
+import Feeds from './Components/Feeds';
 const App = () => {
   return (
-    <Box sx={{backgroundColor:"blue"}} >
-      <Link to='/navbar'>Navbar</Link> 
-      <Link to='/feed'>Feeds</Link>
+    <Box sx={{backgroundColor:"black"}} >
+      <Navbar />
+      <Feeds />
       <Link to='/videos'>Videos</Link>
       <Link to='/channel'>Channels</Link>
       <Link to='/search'>Search</Link>
--- src/Components/Feeds.js
+++ src/Components/Feeds.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { useState,useEffect } from 'react'
+import { Box,Typography,Stack } from '@mui/material'
 const Feeds = () => {
   return (
-    <div>Feeds</div>
+    <Stack sx={{sx:'column', md:'row'}}>
+      <Box sx={{height:{sx: 'auto', md:'92vh'},borderRight: '1px solid blue',px:{sx:0,md:2}}}>
+        <Typography className='copyright' variant='body2' sx={{mt:1.5, color:'white'}}>
+          This is Sidebar
+        </Typography>
+
+      </Box>
+
+    </Stack>
   )
 }
 
