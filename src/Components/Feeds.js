c9a37deefd9c2b63c87550c7320c1c96579fc5bd
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-05-06 23:08:13 +0530

    Feeds

--- src/Components/Feeds.js
+++ src/Components/Feeds.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import { useState,useEffect } from 'react'
 import { Box,Typography,Stack } from '@mui/material'
 const Feeds = () => {
   return (
-    <Stack sx={{sx:'column', md:'row'}}>
-      <Box sx={{height:{sx: 'auto', md:'92vh'},borderRight: '1px solid blue',px:{sx:0,md:2}}}>
-        <Typography className='copyright' variant='body2' sx={{mt:1.5, color:'white'}}>
-          This is Sidebar
-        </Typography>
-
-      </Box>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+    <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+      
+      <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+        Copyright © 2022 JSM Media
+      </Typography>
+    </Box>
 
     </Stack>
   )
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,8 +1,28 @@
+import { Stack } from '@mui/material'
 import React from 'react'
+import { categories } from '../Utils/Constants'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+    {categories.map((category) => (
+      <button>
+        <span>
+          {category.icon}
+        </span>
+        <span>
+          {category.name}
+        </span>
+      </button>
+    ))}
+  </Stack>
   )
 }
 
