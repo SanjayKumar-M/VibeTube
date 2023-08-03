9bfbec66ae9cecbd39247091896469d07a29a34f
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-03 22:01:33 +0530

    vid

--- src/Components/VideoCard.js
+++ src/Components/VideoCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Card,Typography,CardContent,CardMedia } from '@mui/material'
 import { demoThumbnailUrl, demoChannelTitle,demoVideoTitle,demoVideoUrl,demoChannelUrl } from '../Utils/Constants'
+
 const VideoCard = ({video:{id:{videoId},snippet}}) => {
   return (
     <Card>
@@ -10,6 +11,20 @@ const VideoCard = ({video:{id:{videoId},snippet}}) => {
         sx={{width:360, height:180}}  
       />
       </Link>
+      <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl } >
+        <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+        </Typography>
+      </Link>
+      <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
+        <Typography variant="subtitle2" color="gray">
+          {snippet?.channelTitle || demoChannelTitle}
+          {/* <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} /> */}
+        </Typography>
+      </Link>
+    </CardContent>
+
     </Card>
   )
 }
