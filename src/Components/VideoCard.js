4f3384e9dba91661bd5b70364ee055bed3b171a2
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-03 20:27:04 +0530

    video card displayed

--- src/Components/VideoCard.js
+++ src/Components/VideoCard.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
-const VideoCard = () => {
+import { Link } from 'react-router-dom'
+import { Card,Typography,CardContent,CardMedia } from '@mui/material'
+import { demoThumbnailUrl, demoChannelTitle,demoVideoTitle,demoVideoUrl,demoChannelUrl } from '../Utils/Constants'
+const VideoCard = ({video:{id:{videoId},snippet}}) => {
   return (
-    <div>VideoCard</div>
+    <Card>
+      <Link to={videoId ? `/vidoe/${videoId}` : demoVideoUrl}>
+      <CardMedia image={snippet?.thumbnails?.high?.url}  alt={snippet?.title} 
+        sx={{width:360, height:180}}  
+      />
+      </Link>
+    </Card>
   )
 }
 
