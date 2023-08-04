45237bbd88c263a838900f27554391f04ebcdc32
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-04 12:53:46 +0530

    api error fixing

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Typography, Box } from "@mui/material";
 import { useParams } from "react-router-dom";
 
@@ -6,25 +6,31 @@ import { FetchAPI } from "../Utils/FetchAPI";
 import Videos from "./Videos";
 
 const Search = () => {
-  const [videos, setVideos] = useState(null);
+  const [videos, setVideos] = useState([]); // Initialize with an empty array
   const { searchTerm } = useParams();
 
   useEffect(() => {
-    FetchAPI(`search?part=snippet&q=${searchTerm}`)
-      .then((data) => setVideos(data.items))
+    if (searchTerm) { // Check if searchTerm exists before making the API call
+      FetchAPI(`search?part=snippet&q=${searchTerm}`)
+        .then((data) => setVideos(data.items))
+        .catch((error) => {
+          console.error("Error fetching videos:", error);
+          setVideos([]); // Clear videos in case of an error
+        });
+    }
   }, [searchTerm]);
 
   return (
     <Box p={2} minHeight="95vh">
-      <Typography variant="h4" fontWeight={900}  color="white" mb={3} ml={{ sm: "100px"}}>
+      <Typography variant="h4" fontWeight={900} color="white" mb={3} ml={{ sm: "100px" }}>
         Search Results for <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
       </Typography>
       <Box display="flex">
-        <Box sx={{ mr: { sm: '100px' } }}/>
-        {<Videos videos={videos} />}
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos} /> {/* Render Videos component only when videos exist */}
       </Box>
     </Box>
   );
 };
 
-export default Search
+export default Search;
--- src/Utils/FetchAPI.js
+++ src/Utils/FetchAPI.js
@@ -1,24 +1,18 @@
 import axios from "axios";
 
-
 const baseURL = 'https://youtube-v31.p.rapidapi.com';
 
+const headers = {
+  'X-RapidAPI-Key': '1346969e88msh74ebf3189684698p14f3efjsn3a2de6cb2b4c',
+  'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+};
 
-const options = {
-    method: 'GET',
-    params: {
-      relatedToVideoId: '7ghhRHRP6t4',
-      part: 'id,snippet',
-      type: 'video',
-      maxResults: '50'
-    },
-    headers: {
-      'X-RapidAPI-Key': '1346969e88msh74ebf3189684698p14f3efjsn3a2de6cb2b4c',
-      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-    }
-  };
-  
-export const FetchAPI = async(url) =>{
-   const {data}= await axios.get(`${baseURL}/${url}`,options);
-   return data;
-}
+export const FetchAPI = async (url) => {
+  try {
+    const response = await axios.get(`${baseURL}/${url}`, { headers });
+    return response.data;
+  } catch (error) {
+    console.error('API Error:', error);
+    throw error; 
+  }
+};
